2b9ec9b76b555077a3eb6fd452b4118e64ad0084
cristinanico <135840337+cristinanico@users.noreply.github.com>
2023-09-28 19:10:36 +0200

    content outline

---
+++ public/assets/js/main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+//CONSTANTES
+// const de contenedores
+const series = document.querySelector('.js-container');
+const favorites = document.querySelector('.js-favorites');
+// const del form
+const inputText = document.querySelector('.js-input-search');
+const inputBtn = document.querySelector('.js-btn-search');
+
+//el fetch -> api.tvmaze.com/search/shows?q= 
+
+// FUNCIONES
+
+//El fetch tiene que pintar una serie en el HTML
+
+//Función para pintar una serie en el HTML, renderSerie
+
+//Funcion renderSerieList para pintar una serie en el HTML
+
+//Necesito una función que recorra el listado de series y lo pinte en la secction const series con la función renderSerie
+
+//Añadir series FAV
+
+// EVENTOS
+//# sourceMappingURL=main.js.map
